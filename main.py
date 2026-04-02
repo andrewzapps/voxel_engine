58329aa6b709e3f28a196e7466a3cb276bcf4fd7
andrewzapps <your-email@example.com>
2026-04-02 01:08:52 -0400

    added comments

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ import sys
 class voxelEngine:
     def __init__(self):
         pg.init()
+        #create opengl 3.3 context
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+
+        #requests for opengl core profile
         pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+
+        #24 bit depth buffer
         pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE, 24)
 
+        #creates window using gl and double buffering
         pg.display.set_mode(WIN_RES, flags=pg.OPENGL | pg.DOUBLEBUF)
+
+        #opengl context object for gpu
         self.ctx = mgl.create_context()
 
         self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
+        
+        #auto clean memory
         self.ctx.gc_mode = 'auto'
 
         self.clock = pg.time.Clock()
