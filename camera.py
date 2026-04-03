11461e7854cda471c9a8cb89d39dc2862c1a5abd
andrewzapps <your-email@example.com>
2026-04-03 15:43:33 -0400

    camera movement

--- camera.py
+++ camera.py
@@ -13,6 +13,12 @@ class Camera:
         self.m_proj = glm.perspective(V_FOV, ASPECT_RATIO, NEAR, FAR)
         self.m_view = glm.mat4()
 
+    def update(self):
+        self.update_vectors()
+        self.update_view_matrix
+
+    def update_view_matrix(self):
+        self.m_view = glm.lookAt(self.position, self.position + self.forward, self.up)
     
     #recalculate forward vector for camera based on yaw and pitch
     def update_vectors(self):
@@ -24,3 +30,29 @@ class Camera:
         self.forward = glm.normalize(self.forward)
         self.right = glm.normalize(glm.cross(self.forward, glm.vec3(0, 1, 0)))
         self.up = glm.normalize(glm.cross(self.right, self.forward))
+
+    #control camera
+    def rotate_pitch(self, delta_y):
+        self.pitch -= delta_y
+        self.pitch = glm.clamp(self.pitch, -PITCH_MAX, PITCH_MAX)
+
+    def rotate_yaw(self, delta_x):
+        self.yaw += delta_x
+
+    def move_left(self, velocity):
+        self.position -= self.right * velocity
+    
+    def move_right(self, velocity):
+        self.position += self.right * velocity
+
+    def move_up(self, velocity):
+        self.position += self.up * velocity
+
+    def move_down(self, velocity):
+        self.position -= self.up * velocity 
+    
+    def move_forward(self, velocity):
+        self.position += self.forward * velocity 
+    
+    def move_back(self, velocity):
+        self.position -= self.forward * velocity 
